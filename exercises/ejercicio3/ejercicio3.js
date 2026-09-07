cc9a032de6349e31d7d5750f87471e67f823191c
Isaac Alvarez <isaacalvarez673@gmail.com>
2026-09-07 09:38:58 -0300

    Integración de todos los ejercicios de sprint 2 semana 1 con la rama develop

---
+++ exercises/ejercicio3/ejercicio3.js
@@ -0,0 +1,27 @@
+let totalCompra=0;
+let comprando = true;
+let contProductos = 0;
+let conversionPrecio= 0;
+
+while (comprando) {
+
+    let respuesta = confirm("¿Desea agregar un producto al carrito?")
+    
+    if(respuesta){
+        console.log("producto agregado exitosamente.")
+        let precioProducto = prompt("Ingrese el precio del producto")
+        conversionPrecio = parseFloat(precioProducto)
+        if(!isNaN(conversionPrecio)){
+            totalCompra += conversionPrecio
+        }else{
+            alert("Por favor, ingrese un número válido.")
+        }
+        contProductos ++
+        
+    }else{
+        alert("Gracias por visitar nuestro sitio.")
+        comprando = false
+    }
+}
+
+alert(`${contProductos} agregados al carrito, valor ${totalCompra}`)
