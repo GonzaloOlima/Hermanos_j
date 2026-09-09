e5af2f2d8839d1d29a617b0424c833d57e2e74db
Gonzalo Olima <gonzaloolima67@gmail.com>
2026-09-09 02:19:38 -0300

    ejercicio3 s2s2

--- exercises/ejercicio3/ejercicio3.js
+++ exercises/ejercicio3/ejercicio3.js
@@ -1,4 +1,4 @@
-let totalCompra=0;
+/*let totalCompra=0;
 let comprando = true;
 let contProductos = 0;
 let conversionPrecio= 0;
@@ -24,4 +24,25 @@ while (comprando) {
     }
 }
 
-alert(`${contProductos} agregados al carrito, valor ${totalCompra}`)
+alert(`${contProductos} agregados al carrito, valor ${totalCompra}`)*/
+
+let preferenciasUsuario = {
+  tema: "oscuro",
+  idioma: "es",
+  notificacion: true,
+};
+
+let btnGuardar = document.getElementById("btn_guardar");
+let btnCargar = document.getElementById("btn_Cargar");
+
+btnGuardar.addEventListener("click", function () {
+  let preferencias = JSON.stringify(preferenciasUsuario);
+  localStorage.setItem("misPreferencias", preferencias);
+  alert("Preferencias guardadas");
+});
+
+btnCargar.addEventListener("click", function () {
+  let preferencias = localStorage.getItem("misPreferencias");
+  let preferenciasRecuperadas = JSON.parse(preferencias);
+  console.log(preferenciasRecuperadas);
+});
