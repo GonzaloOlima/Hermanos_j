523b4d864fc3ec6ed4c6279246eb065186fae0bd
Isaac Alvarez <isaacalvarez673@gmail.com>
2026-09-10 20:33:22 -0300

    Se comprobó el ejercicio y se movió codigo a otros ficheros

--- exercises/ejercicio3/ejercicio3.js
+++ exercises/ejercicio3/ejercicio3.js
@@ -25,25 +25,3 @@ while (comprando) {
 }
 
 alert(`${contProductos} agregados al carrito, valor ${totalCompra}`)*/
-
-let preferenciasUsuario = {
-    tema: "oscuro",
-    idioma: "es",
-    notificaciones: true, // (Opcional) Corregido a plural como sugería el ejemplo
-};
-
-// Corregido para coincidir con id="btn-guardar" e id="btn-cargar" del HTML
-let btnGuardar = document.getElementById("btn-guardar");
-let btnCargar = document.getElementById("btn-cargar");
-
-btnGuardar.addEventListener("click", function () {
-    let preferencias = JSON.stringify(preferenciasUsuario);
-    localStorage.setItem("misPreferencias", preferencias);
-    alert("Preferencias guardadas");
-});
-
-btnCargar.addEventListener("click", function () {
-    let preferencias = localStorage.getItem("misPreferencias");
-    let preferenciasRecuperadas = JSON.parse(preferencias);
-    console.log(preferenciasRecuperadas);
-});
