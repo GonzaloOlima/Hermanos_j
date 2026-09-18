810e62119e19b9edc1b2f0e4c05b1fbaf0733e64
Isaac Alvarez <isaacalvarez673@gmail.com>
2026-09-18 16:22:50 -0300

    feat: inicializa backend con chalk

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+const chalk = require('chalk');
+
+console.log(chalk.green('Servidor iniciado correctamente'));
+console.log(chalk.blue('Entorno de desarrollo'));
+console.log(chalk.yellow('¡Backend listo para trabajar!'));
