1953f739d4f705182b23752ceb8d9f5aefcdfc6a
Beluluuu <sofibelengerman@gmail.com>
2026-09-23 14:11:10 -0300

    Ejercicio 5 terminado y testeado

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ const { url } = require("inspector");
 
 const server = http.createServer((req, res) => {
     if (req.url == "/"){
-        res.writeHead(200, {"Content-Type": "text/plain"});
-        res.end('<h1>Bienvenido al servidor de Mueblería Jota</h1>');
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end('<h1>Bienvenido al servidor de Muebleria Jota</h1>');
     } else if (req.url == "/productos"){
-        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.writeHead(200, {"Content-Type": "text/html"});
         res.end('<h2>Nuestro catalaogo de productos</h2>');
     } else if (req.url == "/contacto"){
-        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.writeHead(200, {"Content-Type": "text/html"});
         res.end('<p>Contáctanos al 555-1234</p>');
     } else {
         res.writeHead(404, {"Content-Type": "text/plain"});
